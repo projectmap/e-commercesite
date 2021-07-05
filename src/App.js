4508894238ee0872d65aeb0749326774f5e84770
Suren Sedai <suren.sedai9@gmail.com>
2021-07-05 15:12:40 +0545

    shop page created with header and collection item components

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
+import Header from './components/header/header.component';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 const HatsPage = () => (
   <div>
@@ -14,9 +16,11 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+import {ReactComponent as Logo} from "../../assets/crownlogo.svg"
+import "./header.styles.scss";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-holder">
+        <Link className="home" to="/">
+         <Logo/>
+        </Link>
+      </div>
+      <div className="option-holder">
+        <div className="option">
+          <Link className="options" to="/shop">
+            {" "}
+            SHOP
+          </Link>
+        </div>
+        <div className="option">
+          {" "}
+          <Link className="options"> CONTACT</Link>
+        </div>
+        <div className="option">
+          {" "}
+          <Link className="options"> SIGN IN</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
