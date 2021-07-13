313b3dd1c64fad01a0b9114225b3b0c9318e5623
Suren Sedai <suren.sedai9@gmail.com>
2021-07-13 11:00:11 +0545

    updated carticon and cartdrop down component with selector memoization

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,19 +1,34 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
+
+import CartItem from "../cart-item/cart-item.component";
 import "./cart-dropdown.styles.scss";
 
+import { connect } from "react-redux";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 
 
 
-
-const CartDropdown=()=>{
+const CartDropdown=({cartItems})=>{
     return(
-        <div className="cart-dropdown">cart dropdown
+        <div className="cart-dropdown">
 
-<div className="cart-items"></div>
+<div className="cart-items">
+    {cartItems.map((item)=>{
+        return(
+            <CartItem key={item.id} item={item}/>
+        )
+    })}
+</div>
 <Button variant="contained" color="primary">Checkout</Button>
         </div>
     )
 }
 
-export default CartDropdown;
+
+
+const mapStateToProps=(state)=>({
+    cartItems:selectCartItems(state)
+})
+
+export default connect(mapStateToProps)(CartDropdown);
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,28 +4,34 @@ import { setToggleMenu } from "../../redux/cart/cart.actions";
 import {connect} from "react-redux";
 import "./cart-icon.styles.scss";
 
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
+
 import {ReactComponent as ShoppingCart} from "../../assets/shop-cart.svg";
 
 
 
 
-const CartIcon=({setHideMenu,setCartMenu})=>{
+const CartIcon=({setHideMenu,setCartMenu,cartItemCount})=>{
    
     return(
         <div className="cart-icon" onClick={setHideMenu}>
             <ShoppingCart className="shopping-icon"/>
 
-            <span className="item-count">0</span>
+            <span className="item-count">{cartItemCount}</span>
         </div>
     )
 }
 
 
+const mapStateToProps=(state)=>({
+    cartItemCount:selectCartItemsCount(state)
+})
+
 const mapDispatchToProps=(dispatch)=>({
     setHideMenu:()=>dispatch(setToggleMenu())
 })
 
-export default connect(null,mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps,mapDispatchToProps)(CartIcon);
 
 
 
